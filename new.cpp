39987477df49808e7f2f4fc51d84871506437d4a
kudeepjhorar40 <kuldeepjhorar40@gmail.com>
2025-01-20 15:21:11 +0530

    Adding new file

---
+++ new.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void func(){
+
+    return;
+}
+
+int main(){
+    vector<int> arr = {};
+    string s  = "";
+    int a  = 0;
+    func();
+    return  0 ;
+}
